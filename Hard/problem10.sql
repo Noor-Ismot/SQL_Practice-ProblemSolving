d3ef2e3cbfebd57063bf72919d8c474efe5a3b6f
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2026-01-28 23:58:41 +0100

    Solution 10: Hard Problem

--- Hard/problem10.sql
+++ Hard/problem10.sql
@@ -7,4 +7,13 @@ WHERE province_name = 'Ontario'
 UNION ALL
 SELECT province_name
 FROM province_names
-WHERE province_name != 'Ontario';
+WHERE province_name != 'Ontario'
+ORDER BY province_name;
+
+
+--solution 2:--
+SELECT province_name
+FROM province_names
+ORDER BY
+    CASE WHEN province_name = 'Ontario' THEN 0 ELSE 1 END,
+    province_name;
