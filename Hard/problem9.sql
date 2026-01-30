0ec975fad8a4f039522894883bcd20e9d462f8f3
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2026-01-30 23:41:04 +0100

    Solution 9 : Hard Problem

--- Hard/problem9.sql
+++ Hard/problem9.sql
@@ -1,3 +1,9 @@
 --Author: Noor Ismot Ara --
 -- Website: https://www.sql-practice.com/ --
 
+SELECT
+ admission_date,
+ count(admission_date) AS admission_day,
+ count(admission_date) - LAG(count(admission_date)) OVER(ORDER BY admission_date) AS admission_count_change 
+FROM admissions
+GROUP BY admission_date;
